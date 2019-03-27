cd8af9f7c1900f2320b6c09a8f60be92a0be3f29
Denis Lopes <denis.lopes@credsystem.com.br>
2019-03-27 09:02:15 -0300

    first commit

---
+++ Ocomon/Controllers/OcorrenciasController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ocomon.Controllers
+{
+    public class OcorrenciasController : Controller
+    {
+        //
+        // GET: /Ocorrencias/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AbrirChamado()
+        {
+            return View();
+        }
+
+        public ActionResult Consultar()
+        {
+            return View();
+        }
+
+        public ActionResult BuscaRapida()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Ocomon/Controllers/RelatoriosController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ocomon.Controllers
+{
+    public class RelatoriosController : Controller
+    {
+        //
+        // GET: /Relatorios/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Slas()
+        {
+            return View();
+        }
+
+    }
+}
